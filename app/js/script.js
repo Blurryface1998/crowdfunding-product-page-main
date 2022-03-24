de1334d2f07e9f629fef0ed9cd65d6f21d1ec437
Blurryface1998 <83365791+Blurryface1998@users.noreply.github.com>
2022-03-24 15:38:13 +0100

    Part 2

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,13 @@
 let modal = document.getElementById("modal")
 let btn = document.getElementById("modalMenu");
 let span = document.getElementsByClassName("close")[0];
+let modalB = document.querySelector(".main__top--modal");
+let btnOpenModal = document.getElementById("buttonModal");
+let btnClose = document.getElementsByClassName("iconClose")[0];
+let enterPledge = document.getElementsByClassName("enterPledge");
+const body = document.querySelector("body");
+const radioBtn = document.querySelector("#radio");
+
 
 // Modal menu for Nav
 btn.onclick = function() {
@@ -19,3 +26,42 @@ window.onclick = function(event) {
         btn.style.display = "block";
     }
 };
+
+
+btnOpenModal.addEventListener("click", function() {
+    modalB.style.display = "block";
+    body.style.overflow = "hidden";
+});
+
+btnClose.addEventListener("click", function() {
+    modalB.style.display = "none";
+    body.style.overflow = "auto";
+});
+
+window.addEventListener("click", function(event) {
+    if(event.target == modalB) {
+        modalB.style.display = "none";
+        body.style.overflow = "auto";
+    }
+});
+
+
+function toggle(element) {
+    for(let i = 0; i < element.length; i++) {
+        element[i].addEventListener("click", function(e) {
+            let x = this;
+            for (let j = 0; j < element.length; j++) {
+                if(x != element[j]) {
+                    element[j].classList.remove("active");
+                
+                } else if(x.classList.contains("active") === true) {
+                    x.classList.remove("active");
+                } else {
+                    x.classList.add("active");
+                }
+            }
+            e.preventDefault();
+        });
+    }
+}
+toggle(document.querySelectorAll(".parts"));
