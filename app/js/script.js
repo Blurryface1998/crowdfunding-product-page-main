b566fd60a27f05215fe68c367ef9bf4c60095de9
Blurryface1998 <83365791+Blurryface1998@users.noreply.github.com>
2022-03-25 16:01:21 +0100

    Part 3

--- app/js/script.js
+++ app/js/script.js
@@ -4,10 +4,15 @@ let span = document.getElementsByClassName("close")[0];
 let modalB = document.querySelector(".main__top--modal");
 let btnOpenModal = document.getElementById("buttonModal");
 let btnClose = document.getElementsByClassName("iconClose")[0];
-let enterPledge = document.getElementsByClassName("enterPledge");
+let enterPledge = document.querySelectorAll(".enterPledge");
 const body = document.querySelector("body");
-const radioBtn = document.querySelector("#radio");
-
+const radio = document.getElementsByClassName("radioTest");
+const parts = document.querySelectorAll(".parts");
+const continueBtn = document.querySelectorAll(".btnContinue");
+const gotIt = document.getElementById("btn");
+const modalStart = document.querySelector(".main__support");
+const bookmark = document.getElementById("bookmark");
+const demo = document.getElementById("demo");
 
 // Modal menu for Nav
 btn.onclick = function() {
@@ -24,10 +29,18 @@ window.onclick = function(event) {
     if(event.target == modal) {
         modal.style.display = "none";
         btn.style.display = "block";
-    }
+
+    } else if(event.target == modalB) {
+        modalB.style.display = "none";
+        body.style.overflow = "auto";
+
+    } else if(event.target == modalStart) {
+        modalStart.style.display = "none";
+        body.style.overflow = "auto";
+    } 
 };
 
-
+// Modal menu for Becker
 btnOpenModal.addEventListener("click", function() {
     modalB.style.display = "block";
     body.style.overflow = "hidden";
@@ -42,26 +55,59 @@ window.addEventListener("click", function(event) {
     if(event.target == modalB) {
         modalB.style.display = "none";
         body.style.overflow = "auto";
-    }
+    } 
 });
 
 
+
+// For Toggling Radio button to change border style and show Enter Pledge div
 function toggle(element) {
-    for(let i = 0; i < element.length; i++) {
-        element[i].addEventListener("click", function(e) {
-            let x = this;
-            for (let j = 0; j < element.length; j++) {
-                if(x != element[j]) {
-                    element[j].classList.remove("active");
-                
+    for(let a = 0; a < radio.length; a++) {
+        radio[a].addEventListener("click", function(d) {
+            for(let i = 0; i < element.length; i++) {
+                element[i].addEventListener("change", function(e) {
+                    let x = this;
+                    for (let j = 0; j < element.length; j++) {
+                        if(x != element[j]) {
+                            element[j].classList.remove("active");
+                        } else if(x.classList.contains("active") === false) {
+                            x.classList.add("active");
+                        } 
+                        
+                    }
+                    e.preventDefault();
+                    
+                for(let f = 0; f < enterPledge.length; f++) {
+                if(x != element[f]) {
+                    enterPledge[f].classList.remove("display");
                 } else if(x.classList.contains("active") === true) {
-                    x.classList.remove("active");
-                } else {
-                    x.classList.add("active");
+                    enterPledge[f].classList.add("display");
                 }
             }
-            e.preventDefault();
+                });
+            }
         });
     }
 }
 toggle(document.querySelectorAll(".parts"));
+
+// Modal for Start pledge
+gotIt.onclick = function() {
+    modalStart.style.display = "none";
+    body.style.overflow = "auto";
+};
+
+for(let i = 0; i < continueBtn.length; i++) {
+    continueBtn[i].addEventListener("click", function() {
+        modalStart.style.display = "block";
+        modalB.style.display = "none";
+        body.style.overflow = "none";
+    });
+}
+
+// Bokmark change on click
+bookmark.onclick =  function() {
+   demo.classList.add("function");
+
+  
+};
